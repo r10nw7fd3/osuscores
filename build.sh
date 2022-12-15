788df53264b4dbca53d450796da07f9196192518
snr1s <snr1s@outlook.com>
2022-12-15 16:57:23 +0300

    Specify classpath in manifest

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 set -ex
 
 mkdir -p bin/classes
-javac -cp ./lib/*.jar -d bin/classes src/*.java
+javac -cp json.jar -d bin/classes src/*.java
 cd bin/classes
 jar cvfm ../../osuscores.jar ../../MANIFEST.MF *
