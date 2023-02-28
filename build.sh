602a02da55f0b1f0aa76520b766e82d20f9a6a6d
snr1s <snr1s@outlook.com>
2023-02-28 20:13:19 +0300

    Make build.sh output less noisy

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -ex
 mkdir -p bin/classes
 javac -cp json.jar -d bin/classes src/*.java
 cd bin/classes
-jar cvfm ../../osuscores.jar ../../MANIFEST.MF *
+jar cfm ../../osuscores.jar ../../MANIFEST.MF *
